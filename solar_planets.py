4f22ae1c7f36e294bbe7fe69a1ef0676638c7d9c
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2024-02-28 16:16:23 -0300

    Add files via upload

--- solar_planets.py
+++ solar_planets.py
@@ -2,14 +2,14 @@ import cv2
 
 img = cv2.imread("solar-system.jpg")
 
-cv2.putText(img, "Mercurio", (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Venus", (200, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Mercurio", (110, 180), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Venus", (190, 270), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.putText(img, "Terra", (350, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Marte", (500, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Jupiter", (650, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Saturno", (800, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Urano", (950, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(img, "Netuno", (1100, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Marte", (300, 270), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Jupiter", (500, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Saturno", (720, 118), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Urano", (950, 130), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, "Netuno", (1080, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
 cv2.imshow("resultado", img)
 
